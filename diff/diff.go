c312f4c6cef0b53b1dbb0584b3c2e4035ddf7dcb
muakasan <muakasan@gmail.com>
2017-10-25 00:49:28 -0500

    Created file struct

--- diff/diff.go
+++ diff/diff.go
@@ -1,31 +1,45 @@
 package diff
-/*
-import (
-    "io/ioutil"
-)
-*/
+
+//https://stackoverflow.com/questions/1726336/how-do-i-use-a-generic-vector-in-go
 
 type diff struct {
 }
 
 type file struct {
+    lineSlice []string
+}
+
+func (f *file) GetLine(i int) string {
+    return f.lineSlice[i]
+}
+
+func (f *file) SetLine(i int, s string) {
+    f.lineSlice[i] = s
+}
+
+func (f *file) AppendLine(s string) {
+    f.lineSlice = append(f.lineSlice, s)
+}
+
+func (f *file) NumLines() int {
+    return len(f.lineSlice)
 }
 
 func WasModified(basefile file, newfile file) bool {
-    return false;
+    return false
 }
 
 func CreateDiff(basefile file, newfile file) diff {
     //http://www.xmailserver.org/diff2.pdf
-    return diff{};
+    return diff{}
 }
 
 func DiffToSwapfile(d diff) file {
-    return file{};
+    return file{}
 }
 
 func SwapfileToDiff(swapfile file) diff {
-    return diff{};
+    return diff{}
 }
 
 //Not sure what to do here...
