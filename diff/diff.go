81c19a85e114a79beccd2e479345c82be4c73f2b
elc1798 <elc1798@gmail.com>
2017-11-06 15:52:17 -0600

    Fix diff test

--- diff/diff.go
+++ diff/diff.go
@@ -1,38 +1,41 @@
 package diff
 
-import "crypto/md5"
-import "io"
-import "bytes"
+import (
+	"bytes"
+	"crypto/md5"
+	"io"
+
+	fs "github.com/elc1798/teatime/fs"
+)
 
 type diff struct {
 }
 
 func fileHash(f fs.File) []byte {
-    h := md5.New()
-    for i := 0; i < f.NumLines(); i++ {
-        io.WriteString(h, f.GetLine(i))
-    }
-    return h.Sum(nil)
+	h := md5.New()
+	for i := 0; i < f.NumLines(); i++ {
+		io.WriteString(h, f.GetLine(i))
+	}
+	return h.Sum(nil)
 }
 
 func WasModified(basefile fs.File, newfile fs.File) bool {
-    return !bytes.Equal(fileHash(basefile), fileHash(newfile))
+	return !bytes.Equal(fileHash(basefile), fileHash(newfile))
 }
 
 func CreateDiff(basefile fs.File, newfile fs.File) diff {
-    //http://www.xmailserver.org/diff2.pdf
-    return diff{}
+	//http://www.xmailserver.org/diff2.pdf
+	return diff{}
 }
 
 func DiffToSwapfile(d diff) fs.File {
-    return fs.File{}
+	return fs.File{}
 }
 
 func SwapfileToDiff(swapfile fs.File) diff {
-    return diff{}
+	return diff{}
 }
 
 //Not sure what to do here...
-func HandleMergeConflicts(d diff){
+func HandleMergeConflicts(d diff) {
 }
-
